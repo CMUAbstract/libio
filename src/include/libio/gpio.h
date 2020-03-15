45d5357d1e02de9be70ba657bf87664f8d2e2c66
g-ram <ggobieski@thule.andrew.cmu.edu>
2020-03-15 13:45:34 -0400

    qf to gpio

--- src/include/libio/gpio.h
+++ src/include/libio/gpio.h
@@ -1,6 +1,8 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#include <stdint.h>
+
 extern volatile uint32_t pin1_status __attribute__((section(".pin1_status")));
 extern volatile uint32_t pin2_status __attribute__((section(".pin2_status")));
 extern volatile uint32_t pin3_status __attribute__((section(".pin3_status")));
